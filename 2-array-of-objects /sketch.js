cc88007395fca69e925f3d31b60ed8627d0d4ad6
charlottebaszucki <charlottebaszucki@gmail.com>
2020-04-29 11:51:52 -0700

    did some stuff to try to make it work, still doesn't

---
+++ 2-array-of-objects /sketch.js
@@ -0,0 +1,34 @@
+
+//create an empty array called balls
+let me;
+me = new Avatar(width/2, 300, 6);
+
+function draw(){
+	background(220);
+
+    me.drawMe();
+    me.moveMe();
+
+
+
+}
+class Avatar {
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+	drawMe(){  // draw the running person
+    		stroke("gold");
+        strokeWeight(1);
+    		fill("red");
+		    ellipse(this.x,this.y,40,40);
+        line(this.x,this.y, this.x, this.y+40);
+        line(this.x, this.y+40, this.x-20, this.y+60);
+        line(this.x, this.y+40, this.x+10, this.y+50);
+        line(this.x+10, this.y+50, this.x+5, this.y+60);
+        line(this.x, this.y+15, this.x-10, this.y+25);
+        line(this.x-10, this.y+25, this.x+10, this.y+35);
+	}
---
+++ 5-animated-sprites /sketch.js
@@ -0,0 +1,28 @@
+// Creating animations
+//
+// animations like p5 images should be stored in variables
+// in order to be displayed during the draw cycle
+var ghost;
+//
+// //it's advisable (but not necessary) to load the images in the preload function
+// //of your sketch otherwise they may appear with a little delay
+function preload() {
+//
+//   //create an animation from a sequence of numbered images
+//   //pass the first and the last file name and it will try to find the ones in between
+  frog = loadAnimation('sprites/frog001.png', 'sprites/frog015.png');
+
+}
+
+function setup() {
+  createCanvas(800, 700);
+}
+
+function draw() {
+  background(200, 255, 255);
+
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+  animation(frog, 300, 300);
+
+}
