52a4068bfeeafc5fa2d3581654b1d50b25211069
charlottebaszucki <charlottebaszucki@gmail.com>
2020-05-04 12:20:10 -0700

    added shhhhhh and tried to add restart button

--- Angry Librarian/sketch.js
+++ Angry Librarian/sketch.js
@@ -1,30 +1,36 @@
 
-
-  // Creating animations
-  //
-  // animations like p5 images should be stored in variables
-  // in order to be displayed during the draw cycle
   var librarian;
-  //
-  // //it's advisable (but not necessary) to load the images in the preload function
-  // //of your sketch otherwise they may appear with a little delay
+  // let button;
+
+
+
   function preload() {
-  //
-  //   //create an animation from a sequence of numbered images
-  //   //pass the first and the last file name and it will try to find the ones in between
+
     librarian = loadAnimation('sprites/librarian001.png', 'sprites/librarian033.png');
 
   }
 
   function setup() {
     createCanvas(900, 700);
+    // button = createButton('try again');
+    // button.size(250,60);
+    // button.position(300, 200);
+    // button.style("font-size", "35px");
+    // button.style("font-family", "Verdana-Bold ");
+    var button = createButton ("restart");
+    button.mousePressed("resetSketch");
+
+  }
+
+  function resetSketch(){
+
   }
 
   function draw() {
     background(200, 255, 255);
-
-    //specify the animation instance and its x,y position
-    //animation() will update the animation frame as well
-    animation(librarian, 400, 310);
+    animation(librarian, 400, 320);
+    textSize(100);
+    textFont('GillSans-Bold ');
+    text('SHHHHHHH!!', 100, 100);
 
   }
